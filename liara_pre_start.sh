ba4f5819aa13b18c0f8a36ea1ca36020956fe9bb
babaie <h2ohojat@gmail.com>
2025-09-19 01:30:17 +0330

    redy for deploy fix in liara_per_start.sh

--- liara_pre_start.sh
+++ liara_pre_start.sh
@@ -2,5 +2,5 @@ set -e
 echo "Running migrations..."
 python manage.py migrate --noinput
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
 echo "Pre-start hooks finished."
